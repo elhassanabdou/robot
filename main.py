a11b4b1f813d2092200a89a9898ba12f47afd400
elhassanabdou <elhassan.abdou@gmail.com>
2023-01-14 12:47:49 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 220)
+basic.pause(100)
+maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CCW, 220)
---
+++ main.ts
@@ -0,0 +1,3 @@
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 220)
+basic.pause(100)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 220)
